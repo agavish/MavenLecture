c94257ed067d18a6741d82e98cec3c248f403b8a
Adam Gavish <agavish@gmail.com>
2014-12-06 17:12:53 +0200

    added integration tests

--- src/test/java/com/maven/lecture/example/AppIntegrationTest.java
+++ src/test/java/com/maven/lecture/example/AppIntegrationTest.java
@@ -1,18 +1,58 @@
 package com.maven.lecture.example;
 
+import java.io.IOException;
 import java.net.UnknownHostException;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class AppIntegrationTest extends TestCase {
 
+	MongodExecutable	mongodExecutable	= null;
+
+	public AppIntegrationTest() throws IOException {
+		MongodStarter starter = MongodStarter.getDefaultInstance();
+
+		int port = 27017;
+		IMongodConfig mongodConfig = new MongodConfigBuilder()
+				.version(Version.Main.PRODUCTION)
+				.net(new Net(port, false))
+				.build();
+
+		mongodExecutable = starter.prepare(mongodConfig);
+		MongodProcess mongod = mongodExecutable.start();
+
+		MongoClient mongo = new MongoClient("localhost", 27017);
+		DB db = mongo.getDB("numbers");
+		DBCollection col = db.createCollection("numberscollection", new BasicDBObject());
+		col.save(new BasicDBObject("number", 3));
+	}
+
 	/**
-	 * This is an integration test which tests a block of code
-	 * within it's integration with actual inner objects
-	 * @throws UnknownHostException 
+	 * This is an integration test which tests a block of code within it's integration with actual inner objects
+	 * 
+	 * @throws UnknownHostException
 	 */
-	public void testDatabaseIntegration() {
-		Assert.assertTrue(true);
+	public void testDatabaseIntegration() throws UnknownHostException {
+		/**
+		 * No mocks here, real embedded mongoDb is been loaded during the test
+		 */
+		Integer sumTwoRandomNumbersFromDB = App.sumTwoRandomNumbersFromDB(new NumbersDatabaseImpl());
+		Assert.assertNotNull(sumTwoRandomNumbersFromDB);
+
+		if (mongodExecutable != null)
+			mongodExecutable.stop();
 	}
 }
